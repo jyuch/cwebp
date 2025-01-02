a67c21d60488208d9432a99a57a4b15409b3ac41
jyuch <jyuch@users.noreply.github.com>
2025-01-02 12:14:49 +0900

    解像度をコマンドライン引数から与えられるように変更

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use anyhow::Context as _;
 use clap::Parser;
 use image::imageops::FilterType;
-use image::ImageReader;
+use image::{GenericImageView, ImageReader};
 use rayon::iter::ParallelIterator;
 use rayon::prelude::IntoParallelIterator;
 use std::fs;
@@ -10,13 +9,23 @@ use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
+#[clap(disable_help_flag = true)]
 struct Cli {
     /// Input directory.
     #[clap(short, long)]
     input: PathBuf,
+
     /// Output directory.
     #[clap(short, long)]
     output: PathBuf,
+
+    /// Image width.
+    #[clap(short, long)]
+    width: Option<u32>,
+
+    /// Image height
+    #[clap(short, long)]
+    height: Option<u32>,
 }
 
 #[derive(Debug)]
@@ -55,7 +64,7 @@ fn main() -> anyhow::Result<()> {
 
     ci.into_par_iter()
         .map(|it| {
-            let result = convert(&it.input, &it.output);
+            let result = convert(&it.input, &it.output, opt.width, opt.height);
             (it, result)
         })
         .for_each(|it| {
@@ -88,15 +97,21 @@ fn to_item(
     ))
 }
 
-fn convert(input: impl AsRef<Path>, output: impl AsRef<Path>) -> anyhow::Result<()> {
+fn convert(
+    input: impl AsRef<Path>,
+    output: impl AsRef<Path>,
+    width: Option<u32>,
+    height: Option<u32>,
+) -> anyhow::Result<()> {
     let content = fs::read(&input)?;
     let img = ImageReader::new(Cursor::new(&content))
         .with_guessed_format()?
         .decode()?;
 
-    let scale = img.width() as f32 / 720f32;
-    let new_height = img.height() as f32 * scale;
-    let img = img.resize(720u32, new_height as u32, FilterType::Lanczos3);
+    let (cur_width, cur_height) = img.dimensions();
+    let new_width = width.unwrap_or(cur_width);
+    let new_height = height.unwrap_or(cur_height);
+    let img = img.resize(new_width, new_height, FilterType::Lanczos3);
     img.save(output)?;
     Ok(())
 }
